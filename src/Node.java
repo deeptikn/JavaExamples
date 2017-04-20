6739739eb4a9fccc3b5e6aa693c12c7589e4c7b0
Deepti Kashetty Nagaraja <dkashettynagar@expedia.com>
2017-04-20 14:43:31 -0700

    Initial commit

---
+++ src/Node.java
@@ -0,0 +1,15 @@
+/**
+ * Created by dkashettynagar on 4/20/17.
+ */
+public class Node {
+
+        int data;
+        Node right,left,root;
+
+
+        Node(int d){
+            data = d;
+            right = left = root = null ;
+        }
+
+}
